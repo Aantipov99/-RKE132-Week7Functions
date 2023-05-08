fb8ca31b66097f371dd3683547e17717752bb551
Anna Antipova <aantipov@tktk.ee>
2023-05-08 12:52:12 +0300

    create new app

---
+++ functionsWeek7/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Are You coming or leaving? (in or out):*");
+string UserChoice = Console.ReadLine();
+
+if (UserChoice == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+static void PrintHello()
+{
+    Console.WriteLine("Hello,world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later,aligator.");
+}
